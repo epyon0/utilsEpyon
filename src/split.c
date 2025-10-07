4db8d37cc7089186359afafaafa65ddf0cf6c820
epyon0 <epyon0@proton.me>
2025-10-07 16:25:26 -0500

    working on split.c

--- src/split.c
+++ src/split.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include "../lib/libEpyon/misc.h" // git clone git@github.com:epyon0/libEpyon.git
 
+#ifdef WIN32
+#include <io.h>
+#define F_OK 0
+#define access _access
+#endif
+
 char dBuff[1024] = {'\0'};
+char filename[256] = {'\0'};
+bool verboseValue;
+
+int split(FILE *fp);
 
 int main(const int argc, const char *argv[]) {
     if (argc > 1) {
@@ -16,6 +30,89 @@ int main(const int argc, const char *argv[]) {
             const char *arg = argv[i];
             snprintf(dBuff, sizeof(dBuff), "Processing argument: %s", arg);
             verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+
+            if ((strncmp(arg, "-h", sizeof("-h")) == 0) || (strncmp(arg, "--help", sizeof("--help")) == 0)) {
+                printf("\nSplit files or input into chunks\n\n%s <FILE>\nOR\ncat <FILE> | %s\n\n", argv[0], argv[0]);
+                printf("[-h || --help]    Print this help message\n");
+                printf("[-v || --verbose] Turn on verbose output\n");
+                printf("<FILE>            File to process\n");
+                printf("\nReturn Values:\n");
+                printf("  0 = Success\n");
+                printf("  1 = File not accessible\n");
+                printf("  2 = No file or input stream given\n");
+                printf("  3 = Failed opening file\n");
+                printf("  4 = Failed closing file\n");
+
+                printf("\n");
+                return 0;
+            }
+
+
+            if ((strncmp(arg, "-v", sizeof("-v")) == 0) || (strncmp(arg, "--verbose", sizeof("--verbose")) == 0)) {
+                continue;
+            }
+
+            if (access(arg, F_OK) == 0) {
+                strncpy(filename, arg, sizeof(filename));
+                snprintf(dBuff, sizeof(dBuff), "Filename \"%s\" is accessible", arg);
+                verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+            } else {
+                verboseValue = getverbose();
+                setverbose(true);
+                snprintf(dBuff, sizeof(dBuff), "Filename \"%s\" is not accessible", arg);
+                verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+                setverbose(verboseValue);
+                exit(1);
+            }
         }
     }
+
+    if (filename[0] == '\0') {
+        verbose("Filename not given", __FILE__, __LINE__, __FUNCTION__);
+        if (!isatty(0)) {
+            verbose("Detected STDIN", __FILE__, __LINE__, __FUNCTION__);
+            int rc = split(stdin);
+            exit(rc);
+        } else {
+            verboseValue = getverbose();
+            setverbose(true);
+            verbose("No file or input stream given", __FILE__, __LINE__, __FUNCTION__);
+            setverbose(verboseValue);
+            exit(2);
+        }
+    } else {
+        snprintf(dBuff, sizeof(dBuff), "Opening file \"%s\"", filename);
+        verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+
+        FILE *fp = fopen(filename, "rb");
+
+        if (fp == NULL) {
+            verboseValue = getverbose();
+            setverbose(true);
+            snprintf(dBuff, sizeof(dBuff), "Failed opening file \"%s\"", filename);
+            verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+            setverbose(verboseValue);
+            exit(3);
+        }
+
+        int rc = split(fp);
+
+        snprintf(dBuff, sizeof(dBuff), "Closing file \"%s\"", filename);
+        verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+        int err = fclose(fp);
+        if (err != 0) {
+            verboseValue = getverbose();
+            setverbose(true);
+            snprintf(dBuff, sizeof(dBuff), "Failed closing file \"%s\"", filename);
+            verbose(dBuff, __FILE__, __LINE__, __FUNCTION__);
+            setverbose(verboseValue);
+            exit(4);
+        }
+        exit(rc);
+    }
+}
+
+int split(FILE *fp) {
+
+    return(0);
 }
